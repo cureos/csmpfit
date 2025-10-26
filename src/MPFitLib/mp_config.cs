cd0dc1d558b8960bb39ac6673dfaddb6d80e707e
Anders Gustafsson <anders.gustafsson@scandidos.com>
2025-10-26 21:47:43 +0100

    Modernize code #30

--- src/MPFitLib/mp_config.cs
+++ src/MPFitLib/mp_config.cs
@@ -5,7 +5,7 @@
  *   (Argonne National Laboratory, MINPACK project, March 1980)
  * See the file DISCLAIMER for copyright information.
  * 
- * Tranlation to C Language by S. Moshier (moshier.net)
+ * Translation to C Language by S. Moshier (moshier.net)
  * Translation to C# Language by D. Cuccia (http://davidcuccia.wordpress.com)
  * 
  * Enhancements and packaging by C. Markwardt
@@ -30,27 +30,26 @@ namespace MPFitLib
         /* NOTE: the user may set the value explicitly; OR, if the passed
    value is zero, then the "Default" value will be substituted by
    mpfit(). */
-        public double ftol;    /* Relative chi-square convergence criterium Default: 1e-10 */
-        public double xtol;    /* Relative parameter convergence criterium  Default: 1e-10 */
-        public double gtol;    /* Orthogonality convergence criterium       Default: 1e-10 */
-        public double epsfcn;  /* Finite derivative step size               Default: MP_MACHEP0 */
-        public double stepfactor; /* Initial step bound                     Default: 100.0 */
-        public double covtol;  /* Range tolerance for covariance calculation Default: 1e-14 */
-        public int maxiter;    /* Maximum number of iterations.  If maxiter == MP_NO_ITER,
+        public double ftol = 1e-10;    /* Relative chi-square convergence criterion Default: 1e-10 */
+        public double xtol = 1e-10;    /* Relative parameter convergence criterion  Default: 1e-10 */
+        public double gtol = 1e-10;    /* Orthogonality convergence criterion       Default: 1e-10 */
+        public double epsfcn = MPFit.MP_MACHEP0;  /* Finite derivative step size               Default: MP_MACHEP0 */
+        public double stepfactor = 100.0; /* Initial step bound                     Default: 100.0 */
+        public double covtol = 1e-14;  /* Range tolerance for covariance calculation Default: 1e-14 */
+        public int maxiter = 200;    /* Maximum number of iterations.  If maxiter == MP_NO_ITER,
                              then basic error checking is done, and parameter
                              errors/covariances are estimated based on input
                              parameter values, but no fitting iterations are done.  
 		                     Default: 200 */
-        public int maxfev;     /* Maximum number of function evaluations, or 0 for no limit
+        public int maxfev = 0;     /* Maximum number of function evaluations, or 0 for no limit
 		            Default: 0 (no limit) */
-        public int nprint;     /* Default: 1 */
-        public int douserscale;/* Scale variables by user values?
+        public int nprint = 1;     /* Default: 1 */
+        public int douserscale = 0;/* Scale variables by user values?
 		            1 = yes, user scale values in diag;
 		            0 = no, variables scaled internally (Default) */
-        public int nofinitecheck; /* Disable check for infinite quantities from user?
+        public int nofinitecheck = 0; /* Disable check for infinite quantities from user?
 			        0 = do not perform check (Default)
 			        1 = perform check 
 		             */
-        //mp_iterproc iterproc; /* Placeholder pointer - must set to 0 */
     }
 }
